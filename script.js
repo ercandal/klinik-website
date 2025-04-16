80aa78bc79258bb95d14fac18ae236ad82f8e15a
ercandall <ercandall@hotmail.com>
2025-04-16 22:59:08 +0300

    temel olduk

---
+++ script.js
@@ -0,0 +1,5 @@
+// Scroll animasyonu veya dinamik etki istiyorsan buradan başlarsın
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Klinik web sitesi yüklendi.");
+  });
+  
